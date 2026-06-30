58ccf6a129f6109564c1b19d6766480e9d594d1b
cybershivam <raynavin20@gmail.com>
2026-07-01 01:05:48 +0530

    ARRAY

---
+++ array.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int arr[]={1,2,3,4,5,6,7,8,9};
+    int i;
+    for ( i = 8; i>=0 ; i--)
+    {
+        printf("reverce no=%d\n",arr[i]);
+    }
+    
+
+    return 0;
+}
